0d7529a8d6accb45d011773f539a3cf9b95acc78
Tran Tien <titihx19@gmail.com>
2019-07-25 17:36:16 +0700

    add siema carousel

--- wp-content/themes/thienthanhtea/frontend_src/src/js/main.js
+++ wp-content/themes/thienthanhtea/frontend_src/src/js/main.js
@@ -1,7 +1,11 @@
 import { header } from './header'
 import { footer } from './footer'
+import Carousel from './carousel'
 
 window.addEventListener('DOMContentLoaded', function () {
   header()
   footer()
+
+  const carouselHome = document.querySelector('#carousel-home')
+  if (carouselHome) new Carousel(carouselHome, { autoPlay: true }).init()
 })
