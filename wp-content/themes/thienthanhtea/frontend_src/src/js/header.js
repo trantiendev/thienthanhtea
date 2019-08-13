79916d36bee17b6f3fc9018ba758bf706abfac8a
Tran Tien <titihx19@gmail.com>
2019-08-13 10:18:41 +0700

    header js

--- wp-content/themes/thienthanhtea/frontend_src/src/js/header.js
+++ wp-content/themes/thienthanhtea/frontend_src/src/js/header.js
@@ -1,3 +1,32 @@
 export const header = () => {
-  console.log('header js file')
+  const navbar = document.querySelector('.navbar')
+  const hamburger = navbar.querySelector('.navbar-collapse')
+  const dropDown = navbar.querySelector('.dropdown')
+
+  window.addEventListener('resize', onHandleClickMenu)
+  onHandleClickMenu()
+
+  function onHandleClickMenu() {
+    window.innerWidth < 768 ? addEventClick() : removeEventClick()
+  }
+
+  function addEventClick() {
+    hamburger.addEventListener('click', navbarCollapse)
+    dropDown.addEventListener('click', dropdownMenu)
+  }
+
+  function removeEventClick() {
+    navbar.classList.remove('is-active')
+    dropDown.classList.remove('is-active')
+    hamburger.removeEventListener('click', navbarCollapse)
+    dropDown.removeEventListener('click', dropdownMenu)
+  }
+
+  function navbarCollapse() {
+    navbar.classList.toggle('is-active')
+  }
+
+  function dropdownMenu() {
+    dropDown.classList.toggle('is-active')
+  }
 }
