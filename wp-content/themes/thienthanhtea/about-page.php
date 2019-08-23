7abad0e9deefe5545106caa8d58bff02cf4cf4df
Tran Tien <titihx19@gmail.com>
2019-08-23 12:02:18 +0700

    add footer link && setup about page

---
+++ wp-content/themes/thienthanhtea/about-page.php
@@ -0,0 +1,6 @@
+<?php
+/* Template Name: About Us Page */
+  get_header();
+?>
+<div
+<?php get_footer();?>
--- wp-content/themes/thienthanhtea/footer.php
+++ wp-content/themes/thienthanhtea/footer.php
@@ -13,22 +13,29 @@
             <nav class="footer-nav">
               <ul class="footer-link-list">
                 <li class="footer-link-list-item">Products</li>
-                <li class="footer-link-list-item"><a href="#">Link</a></li>
-                <li class="footer-link-list-item"><a href="#">Link</a></li>
-                <li class="footer-link-list-item"><a href="#">Link</a></li>
-                <li class="footer-link-list-item"><a href="#">Link</a></li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/teas/' ) ?>">Tea</a></li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/teaware/' ) ?>">Teaware</a></li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/gift/' ) ?>">Gift</a></li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/feature/' ) ?>">Featured</a></li>
               </ul>
               <ul class="footer-link-list">
-                <li class="footer-link-list-item">Products</li>
-                <li class="footer-link-list-item"><a href="#">FAQ</a></li>
-                <li class="footer-link-list-item"><a href="#">Contact Us</a></li>
+                <li class="footer-link-list-item">Company</li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/about-us/' ) ?>">About Us</a></li>
+              </ul>
+              <ul class="footer-link-list">
+                <li class="footer-link-list-item">Community</li>
+                <li class="footer-link-list-item"><a href="<?php echo home_url( '/news/' ) ?>">News</a></li>
               </ul>
             </nav>
           </div>
           <div class="col col-3 col--md-3 col--sm-12">
             <ul class="footer-link-list">
-              <li class="footer-link-list-item">Products</li>
+              <li class="footer-link-list-item">Address 1</li>
               <li class="footer-link-list-item">Địa chỉ : 512 Trần Phú - TP Bảo Lộc - Lâm Đồng - Việt Nam</li>
+            </ul>
+            <ul class="footer-link-list u-mt-16 u-no-ml">
+              <li class="footer-link-list-item">Address 2</li>
+              <li class="footer-link-list-item">S6 ba vì, phường 15 quận 10, TP HCM </li>
             </ul>
           </div>
         </div>
