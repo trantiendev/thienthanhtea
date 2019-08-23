0cb7b656a5ce213fa1b07486f48c5e69920061b5
Tran Tien <titihx19@gmail.com>
2019-08-23 11:29:05 +0700

    404 page

---
+++ wp-content/themes/thienthanhtea/404.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+<div class="main">
+  <section class="section section-lg has-padding-top">
+    <div class="container">
+      <div class="page-title">
+        <h1 class="headline-xxl">404</h1>
+        <p class="page-title-description"><a href="<?php echo get_option("siteurl"); ?>">Trở lại trang chủ</a></p>
+        <p class="u-mt-16">Nothing to doing here</p>
+      </div>
+    </div>
+  </section>
+  <?php get_footer('sup');?>
+</div>
+<?php get_footer();?>
