e2c706b0341646cb9768c762181c811923024026
Tran Tien <titihx19@gmail.com>
2019-08-24 12:39:38 +0700

    fix 404 page

--- wp-content/themes/thienthanhtea/404.php
+++ wp-content/themes/thienthanhtea/404.php
@@ -3,9 +3,9 @@
   <section class="section section-lg has-padding-top">
     <div class="container">
       <div class="page-title">
-        <h1 class="headline-xxl">404</h1>
-        <p class="page-title-description"><a href="<?php echo get_option("siteurl"); ?>">Trở lại trang chủ</a></p>
-        <p class="u-mt-16">Nothing to doing here</p>
+        <h1 class="headline-xl">Whoops, that page is gone</h1>
+        <p class="page-title-description">Clicked here to go back to homepage</p>
+        <a class="btn btn-primary u-mt-40 w-276" href="<?php echo get_option("siteurl"); ?>">Go to Homepage</a>
       </div>
     </div>
   </section>
