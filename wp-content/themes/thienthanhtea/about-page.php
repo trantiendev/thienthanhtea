725fdefa47cfb9c843b1d1ee74b747f143de7075
Thuannguyen13 <kenzilian13@gmail.com>
2019-09-22 13:43:16 +0700

    Renew PR

--- wp-content/themes/thienthanhtea/about-page.php
+++ wp-content/themes/thienthanhtea/about-page.php
@@ -3,7 +3,163 @@
   get_header();
 ?>
 <div class="main">
+<section class="section section-lg has-padding-top">
+    <div class="container">
+      <div class="row">
+        <div class="col col-8 col-offset-2 col-md-8 col-md-offset-2">
+          <div class="post">
+            <img class="post-image" src="https://media.giphy.com/media/964zGz0ijfBqE/giphy.gif" alt="">
+            <div class="row">
+              <div class="col col-10 col-offset-1">
+                <div class="content">
+                  <h3 class="content-title content-title-xl">THIÊN THÀNH KIM LỘC TRÀ<br>ĐỊA TỰU VƯỢNG TÀI GIA.</h3>
+                  <p class="content-text">Hai chữ “ Thiên Thành” là từ ghép của 2 chữ đầu vế đối lại với nhau mang nghĩa thành công theo lẽ trời.</p>
+                  <p class="content-text">Ý nghĩa của biểu tượng (logo): Lúc mới thành lập, công ty dùng biểu tượng Cô Tiên làm hình ảnh đại diện cho sản phẩm. Sau này, logo chuyển thành hình con nai vàng vì thế nên sản phẩm trà Thiên Thành cũng có lúc còn gọi là Trà Nai Vàng. Hình tượng Nai Vàng gợi nhớ về một thuở xa xưa của đất Blao hoang sơ, trù phú. Vì thế biểu tượng Trà Nai Vàng cũng chính là biểu tượng chung cho cả xứ chè Blao.</p>
+                  <p class="content-text"><strong>Trần Đại Bình – CEO, Công Ty TNHH Trà Thiên Thành</strong>:“Nhìn lại chặng đường hơn 70 năm của công ty trà thiên thành có thể thấy lịch sử trà thiên thành song hành với giai đoạn thăng trầm nhất của đất nước việt nam ta. Bắt nguồn từ truyền thống văn hóa gia đinh, trải qua những khó khăn, thử thách nhưng bằng nhiệt huyết, say mê của những người yêu chè chúng tôi đã ngày càng phát triển. Và, với một niềm tin cháy bỏng, chúng tôi còn muốn viết tiếp lịch sử của công ty bằng những bước tiến dài trong tương lai, mang cây chè bảo lộc đi vươn xa khắp năm châu để tự hào rằng: đây là chè Việt Nam.”</p>
+                  <h3 class="content-title content-title-lg">Lịch sử hình thành của công ty</h3>
+                  <p class="content-text">Cty TNHH Trà Thiên Thành có truyền thống sản xuất Trà Ướp Hương lâu đời từ năm 1945, trà được sản xuất chủ yếu cho lái buôn xuất sang các nước hoặc tiến vua từ đất Bình Định với nhãn hiệu Thiên Thành với con dấu có hình cô Tiên. Trãi qua thời gian chiến tranh nghề trà hương hoa tiến cung đã mai một. Mãi đến 1975 khi nước được hòa bình, nghề trà hương đã dần dần được những người kế nghiệp khôi phục lại và ngày một phát triển cho đến ngày nay.</p>
+                  <p class="content-text">Trà Thiên Thành với nhãn hiệu Nai Vàng đã hình thành và gắn bó với lịch sử hình thành vùng Trà B’Lao từ những ngày đầu tiên. Từ những loại trà truyền thống như: trà xanh, trà xanh ướp hương Thảo Mộc, trà xanh ướp hoa Sói, Ngâu. Sau năm 1983 khi được tự do thương mại hóa, công ty Trà Thiên Thành đã không ngừng nghiên cứu và phát triển thêm các dòng sản phẩm mới.</p>
+                  <p class="content-text">Đến năm 1997 được tỉnh Lâm Đồng chính thức cấp giấy chứng nhận đăng ký kinh doanh Số: 109GP/TLDN cấp : 05/11/1997 Nghề: Chế biến trà ướp hương</p>
+                </div>
+              </div>
+            </div>
 
+            <div class="post-thumbnail">
+              <div class="row">
+                <div class="col col-12">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_1.jpg" alt="Image">
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col col-10 col-offset-1">
+                <div class="content">
+                  <p class="content-text">Đến năm 2010 công ty Thiên Thành đã đầu tư nhà máy để sản xuất thêm dòng trà trà Xanh viên va dòng Oolong để đáp ứng nhu cầu mới của thị trường. Với kinh nghiệm sản xuất các loại trà cho người tiêu dùng trong nước, công ty chúng tôi đảm bảo mang đến cho quý khách những loại trà phù hợp với gu thưởng thức của người Việt.</p>
+
+                  <p class="content-text">Đến năm 2014 xin chuyển thể từ DN Tư Nhân lên Công Ty TNHH Trà Thiên Thành. Kinh doanh: Trà đen, trà xanh, trà ướp hương, trà ooLong.Với diện tích trên 2 hecta, được xây dựng để phục vụ khách du lịch trong cả nước đến tham quan và thưởng thức trà. Đã tiếp đón bao nhiều đoàn khách trong cả nước tại: 336 Trần Phú – xã Lộc Châu – TP Bảo Lộc</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="post-thumbnail">
+              <div class="row">
+                <div class="col col-12">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_2.jpg" alt="Image">
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col col-10 col-offset-1">
+                <div class="content">
+                  <p class="content-text">Trà Thiên Thành với nhãn hiệu Nai Vàng đã hình thành và gắn bó với lịch sử vùng Trà B’Lao từ những ngày đầu tiên. Từ những loại trà truyền thống như: trà xanh, trà xanh ướp Thảo Mộc, trà xanh ướp hoa Sói, Ngâu. Với sự đam mê trà hương, Nai Vàng đã đưa ra thị trường Việt Nam thêm các loại trà với nhiều hương thơm và khẩu vị khác nhau để có thể đáp ứng nhu cầu thường thức đa dạng của người tiêu dùng trong và ngoài nước.</p>
+
+                  <p class="content-text">Công Ty TNHH Trà Thiên Thành luôn giữ vị trí đứng đầu về sản phẩm trà ướp hương trong nhiều năm qua. Với bí quyết sản xuất trà xanh trên cao nguyên Bảo Lộc và nhiều loại thảo mộc, hương hoa tự nhiên, Thiên Thành luôn mang đến cho bạn những tách trà không những đậm đà hương thơm mà còn vô cùng bổ dưỡng.</p>
+
+                  <p class="content-text">Thiên Thành không chỉ hướng đến nhu cầu thưởng thức trà của khách hàng mà còn không ngừng nghiên cứu phát triển nhiều dòng sản phẩm khác nhằm đáp ứng tốt nhất về nhu cầu giải khát và đặc tính bổ dưỡng trong từng lá trà. Bên cạnh đó, Trà Thiên Thành còn cung cấp và gia công chế biến và đóng gói nhiều loại trà thượng hạng như: Trà Oolong, Trà xanh, Trà đen, Lục trà, Hồng trà, trà Thảo Mộc.... và sản xuất theo yêu cầu đặc biệt của khách hàng.</p>
+
+                  <p class="content-text">Thiên Thành vinh dự được nhà nước phong tặng nhiều danh hiệu cao quý như: Giải Thưởng Chất Lượng Vàng, Viet Nam Bestfood - Thực phẩm chất lượng an toàn vì sức khỏe cộng đồng, huy chương vàng trà sen - lài - dứa.  Bên cạnh đó, Thiên Thành có một thị trường phân phối trà trải dài từ bắc vào nam và thường xuyên nhận được những lời khen ngợi về chát lượng sản phẩm mới như: Trà Sâm Dứa, trà sâm dứa mật ong, trà Oolong túi lọc, trà sâm dứa túi lọc.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col col-10 col-offset-1">
+                <div class="content">
+                  <h3 class="content-title content-title-lg">Giải thưởng và chứng nhận</h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="post-thumbnail">
+              <div class="row">
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_3.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_4.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_5.jpg" alt="Image">
+                </div>
+              </div>
+            </div>
+            <p class="post-caption">Chứng nhận đầu tiên 1982 và Chứng nhận CHẤT LƯỢNG VÀ NHÃN HIỆU SẢN PHẨM số:47/202M-89. Cấp ngày 26-09-1989</p>
+
+            <div class="post-thumbnail">
+              <div class="row">
+                
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_6.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_7.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_8.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_9.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_10.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_11.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_12.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-5">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_13.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-3">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_14.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_16.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-6">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_17.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_18.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_19.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_20.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_21.jpg" alt="Image">
+                </div>
+                <div class="col col-md-12 col-4">
+                  <img src="<?php bloginfo('template_directory');?>/frontend_src/public/img/img_about_22.jpg" alt="Image">
+                </div>
+
+              </div>
+            </div>
+            <p class="post-caption">Chứng nhận khen tặng của 10 năm gần đây</p>
+
+            <div class="row">
+              <div class="col col-10 col-offset-1">
+                <div class="content">
+                  <h3 class="content-title content-title-lg">Sản phẩm và dịch vụ</h3>
+                  <h4 class="content-title content-title-sm">1.Chuyên sản xuất, gia công và bán các sản phẩm trong nước</h4>
+                  <p class="content-text">Sản xuất và gia công sản phẩm trong nước bắt nguồn từ truyền thống gia đình, với ưu thế về vùng nguyên liệu và sự vượt trội về công nghệ, công ty chúng tôi đã trở thành một trong những doanh nghiệp đi đầu về ngành trà ở nước ta. Khởi đầu bằng những sản phẩm trà ướp hương còn đơn giản, với sự trỗi dậy không ngừng, các sản phẩm trà của chúng tôi ngày càng đa dạng, phong phú và được phân bố thị trường khắp cả nước. Đi đôi với việc sản xuất trà, chúng tôi còn giúp đỡ các doanh nghiệp bạn trong việc gia công trà. Việc làm này xuất phát từ sự mong muốn thúc đẩy sự liên kết, phát triển ngành trà của cả nước trong bối cảnh hội nhập thương mại ngày càng sâu, rộng và một khát khao rằng rồi ngày mai, cây chè của việt nam sẽ vươn ra xa mãi.</p>
+                  <h4 class="content-title content-title-sm">2.Xuất khẩu các sản phẩm, nguyên liệu trà cho các đối tác nước ngoài</h4>
+                  <p class="content-text">Không chỉ cung cấp cho thị trường trong nước, những loại: như trà đen, matcha, trà ô long của chúng tôi đã được xuất khẩu sang các thị trường lớn như Châu Âu, Nhật Bản thậm chí cả Đài Loan, Trung Quốc (kinh đô của ngành chè thế giới). Hiểu được những khó khăn của việc “mang chuông đi đánh xứ người” cho nên chúng tôi luôn luôn đổi mới không ngừng công nghệ nhưng vẫn giữ nguyên chất lượng và hương vị trà để cho sản phẩm Trà Nai Vàng có thể bay đi xa hơn nữa trên đôi cánh của sự sáng tạo, tâm huyết và truyền thống văn hoá dân tộc.Phương Châm: “Không những chất lượng tốt nhất; Mà còn là thỏa mãn tốt nhất khẩu vị của từng khách hàng – từng vùng – từng giới…”.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   <?php get_footer('sup');?>  
 </div>
 <?php get_footer();?>
