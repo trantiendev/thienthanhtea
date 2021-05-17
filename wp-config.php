c8e6af203a288bbdbbb2b8e8fdb09b55b8d5d6b3
TienLe <trantiendev@gmail.com>
2021-05-17 17:35:29 +0700

    turn off debug log

--- wp-config.php
+++ wp-config.php
@@ -81,7 +81,7 @@ $table_prefix = 'wp_';
 define('WP_DEBUG', true);
 
 // Enable Debug logging to the /wp-content/debug.log file
-define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_LOG', false);
 
 // Disable display of errors and warnings 
 define('WP_DEBUG_DISPLAY', false);
